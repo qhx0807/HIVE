3680ade491bd00cfb37989124e89f3a2f0d0e235
qhx0807 <820240134@qq.com>
2017-12-10 22:38:00 +0800

    :art: update catalog

---
+++ examples/router/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/test',
+      name: 'test',
+      component: r => require.ensure([], () => r(require('../docs/test.md')))
+    }
+  ]
+})
