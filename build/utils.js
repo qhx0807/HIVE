3ff27971fe6e501994e61fa923188abf362d5eba
qhx0807 <820240134@qq.com>
2017-12-11 13:02:21 +0800

    :wrench: :wrench: update config 'md loader'

--- build/utils.js
+++ build/utils.js
@@ -82,6 +82,18 @@ exports.styleLoaders = function (options) {
   return output
 }
 
+exports.wrapCustomClass = function (render) {
+  return function (...args) {
+    return render(...args)
+      .replace('<code class="', '<code class="hljs ')
+      .replace('<code>', '<code class="hljs">')
+  }
+}
+
+exports.convertHtml = function (str) {
+  return str.replace(/(&#x)(\w{4});/gi, $0 => String.fromCharCode(parseInt(encodeURIComponent($0).replace(/(%26%23x)(\w{4})(%3B)/g, '$2'), 16)))
+}
+
 exports.createNotifierCallback = () => {
   const notifier = require('node-notifier')
 
