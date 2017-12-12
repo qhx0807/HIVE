147eff85e123c353af4601a83cbd01d25c4626e4
qhx0807 <820240134@qq.com>
2017-12-12 14:59:02 +0800

    :sparkles: add gulp to pack less

---
+++ build/build-style.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const gulp = require('gulp')
+const cleanCSS = require('gulp-clean-css')
+const less = require('gulp-less')
+const rename = require('gulp-rename')
+const autoprefixer = require('gulp-autoprefixer')
+
+// 编译less
+gulp.task('css', function() {
+  gulp
+    .src('../packages/theme-default/index.less')
+    .pipe(less())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions', 'ie > 8']
+      })
+    )
+    .pipe(cleanCSS())
+    .pipe(rename('hive.css'))
+    .pipe(gulp.dest('../packages/theme-default/lib'))
+})
+
+// 拷贝字体文件
+gulp.task('fonts', function() {
+  gulp
+    .src('../src/styles/common/iconfont/fonts/*.*')
+    .pipe(gulp.dest('../packages/theme-default/lib/fonts'))
+})
+
+gulp.task('build', ['css', 'fonts'])
+
+gulp.task('watch', function() {
+  gulp.watch('../packages/theme-default/*.less', ['css'])
+})
