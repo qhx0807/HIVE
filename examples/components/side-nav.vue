0f489b35a5085ce29c26976ea358a04960504ddc
qhx0807 <820240134@qq.com>
2017-12-14 11:39:38 +0800

    add grid layout

--- examples/components/side-nav.vue
+++ examples/components/side-nav.vue
@@ -6,7 +6,7 @@
         <router-link :class="$route.name===nav.name ? 'active' : ''" v-if="nav.name" :to="{name: nav.name}">{{nav.desc}}</router-link>
         <p v-else class="side-nav-group">{{nav.desc}}</p>
         <div v-for="item in nav.items" :key="item.name">
-          <router-link :to="{name: item.name}" :class="$route.name===item.name ? 'active' : ''" class="slid-nav-component">{{item.desc}}</router-link>
+          <router-link :to="{name: item.name}" :class="$route.name===item.name ? 'active' : ''" class="slid-nav-component"><span class="item-name">{{item.name}}</span>{{item.desc}}</router-link>
         </div>
       </div>
     </div>
@@ -70,6 +70,10 @@ export default {
       padding: 6px 24px 6px 32px;
       color: #616367;
       font-size: 14px;
+      .item-name{
+        text-transform: capitalize;
+        padding-right: 6px;
+      }
     }
     .active {
       color: #3faaf5;
