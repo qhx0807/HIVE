e7347a99566e587e3325bd6c6e4bb79d359145cb
qhx0807 <820240134@qq.com>
2017-12-15 17:24:35 +0800

    feat: button.vue

--- packages/components/button/button.vue
+++ packages/components/button/button.vue
@@ -1,5 +1,7 @@
 <template>
-  <button><slot></slot></button>
+  <button :class="classes" :disabled="disabled" @click="onHandleClick">
+    <slot></slot>
+  </button>
 </template>
 
 <script>
@@ -18,9 +20,42 @@ export default {
       type: String,
       default: ''
     },
-    plain: Boolean,
     disabled: Boolean,
-    round: Boolean
+    shape: String,
+    long: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      prefixCls: 'ive-btn',
+      isIcon: true,
+    }
+  },
+  computed: {
+    classes(){
+      return [
+        `${this.prefixCls}`,
+        {
+          [`${this.prefixCls}-${this.type}`]: !!this.type,
+          [`${this.prefixCls}-long`]: this.long,
+          [`${this.prefixCls}-${this.size}`]: !!this.size,
+          [`${this.prefixCls}-${this.shape}`]: !!this.shape,
+          [`${this.prefixCls}-loading`]: this.loading != null && this.loading,
+          [`${this.prefixCls}-icon-only`]: !this.isIcon && (!!this.icon || this.loading)
+        }
+      ]
+    }
+  },
+  methods: {
+    onHandleClick(event){
+      this.$emit('click', event);
+    }
   }
 }
 </script>
