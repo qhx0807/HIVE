231ffc71129561aaf6226fca3f62f41019ce8d68
qhx0807 <820240134@qq.com>
2017-12-16 23:51:49 +0800

    :lipstick: feat: Icon

---
+++ packages/components/icon/icon.vue
@@ -0,0 +1,37 @@
+<template>
+  <i :class="classes" :style="styles"></i>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    type: String,
+    size: [Number, String],
+    color: String
+  },
+  computed: {
+    classes () {
+      return [
+        `iconfont`,
+        `${this.type}`
+      ]
+    },
+    styles () {
+      let ret = {}
+
+      if (this.size) {
+        ret.fontSize = `${this.size}px`
+      }
+
+      if (this.color) {
+        ret.color = this.color
+      }
+
+      return ret
+    }
+  }
+}
+</script>
+
+
