f1dcecf9add4378e592b1843070da697ac1b9e4e
qhx0807 <820240134@qq.com>
2017-12-16 23:51:26 +0800

    :lipstick: feat: iconfont

--- packages/index.js
+++ packages/index.js
@@ -1,8 +1,9 @@
 import iButton from './components/button'
 import Row from './components/row'
 import iCol from './components/col'
+import Icon from './components/icon'
 
-const components = [iButton, Row, iCol]
+const components = [iButton, Row, iCol, Icon]
 
 const install = function (Vue) {
   if (install.installed) return
@@ -17,5 +18,6 @@ export default {
   install,
   iButton,
   Row,
-  iCol
+  iCol,
+  Icon
 }
