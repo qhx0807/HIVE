c3a5b32a30950d0aebaa05ab040b56675e6d1f52
qhx0807 <820240134@qq.com>
2017-12-31 18:26:59 +0800

    :rocket: update progress

--- examples/docs/progress.md
+++ examples/docs/progress.md
@@ -3,12 +3,30 @@
 #### 基础用法
 
 <div class="demo-block">
-  <i-progress></i-progress>
+  <i-progress width="400" :percent="79"></i-progress>
+  <br>
+  <br>
+  <i-progress width="400" :percent="100"></i-progress>
+  <br>
+  <br>
+  <i-progress width="400" :percent="79" error></i-progress>
 </div>
 
-
 ::: demo
+
 ```html
-123
+<i-progress width="400" :percent="79"></i-progress>
+<i-progress width="400" :percent="100"></i-progress>
+<i-progress width="400" :percent="79" error></i-progress>
 ```
+
 :::
+
+#### Progress Attributes
+
+| 参数    | 说明                   | 类型          | 默认值 |
+| ------- | ---------------------- | ------------- | ------ |
+| width   | progress 宽度，单位 px | String,Number | 100%   |
+| height  | progress 高度，单位 px | String,Number | 8      |
+| percent | progress 进度          | Number        | 0      |
+| error   | 错误状态               | Boolean       | false  |
--- packages/components/progress/progress.vue
+++ packages/components/progress/progress.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="ive-progress-wrap">
-    <div class="ive-progress-outer"></div>
+  <div class="ive-progress-wrap" :style="wpStyle">
+    <div class="ive-progress-outer">
+      <div class="ive-progress-inner">
+        <div :class="bgClass" :style="bgStyle"></div>
+      </div>
+    </div>
+    <span :class="textClass">
+      <Icon v-if="percent>=100 && !error" type="icon-checkmark"></Icon>
+      <span v-if="percent<=100 && !error">{{percent}}%</span>
+      <Icon v-if="error" type="icon-closecircled"></Icon>
+    </span>
   </div>
 </template>
 
@@ -10,10 +19,61 @@ export default {
   props: {
     width: {
       type: [Number, String]
+    },
+    height:{
+      type:[Number, String],
+      default: 8
+    },
+    percent: {
+      type: Number,
+      default: 0,
+      validator: function (value) {
+        return value<=100 && value>=0
+      }
+    },
+    error:{
+      type: Boolean,
+      default: false
     }
   },
   data() {
-    return {}
+    return {
+      prefixCls:'ive-progress'
+    }
+  },
+  computed: {
+    textClass(){
+      return `${this.prefixCls}-text`
+    },
+    bgClass(){
+      return [
+        `${this.prefixCls}-inner-bg`,
+        {
+          [`success`] : this.percent>=100,
+          [`pending`] : this.percent<100 && !this.error,
+          [`error`] : this.error
+        }
+      ]
+    },
+    bgStyle(){
+      let w = this.percent
+      if(w>100){
+        w=100
+      }else if(w<0){
+        w=0
+      }
+      return {
+        width: `${w}%`,
+        height: `${this.height}px`
+      }
+    },
+    wpStyle(){
+      if(this.width) {
+        return {
+          width: `${this.width}px`
+        }
+      }
+    }
   },
   methods: {
     fn() {
