fbfa54d0f3300c1763547a448598de5a2d742a71
qhx0807 <820240134@qq.com>
2018-01-03 16:32:49 +0800

    upload

---
+++ packages/components/upload/img-cropper-upload.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="ive-upload-wrap">
+    <div class="ive-upload-img">
+      <img id="image" :src="imgurl">
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Cropper from 'cropperjs'
+import '../../../node_modules/cropperjs/dist/cropper.min.css';
+export default {
+  name: 'Upload',
+  props: {
+    imgurl: {
+      type: String
+    },
+  },
+  data() {
+    return {
+      cropper: {},
+    }
+  },
+  created() {},
+  mounted() {
+    this.initCrop()
+  },
+  methods: {
+    initCrop() {
+      let img = document.getElementById('image')
+      this.cropper = new Cropper(img, {
+        preview:'#preview'
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
+
--- packages/components/upload/upload.vue
+++ packages/components/upload/upload.vue
@@ -2,14 +2,16 @@
   <div class="ive-upload-wrap">
     <input type="file" ref="input" :multiple="multiple" @change="change">
     <i-button type="primary" @click="handleClick" icon="icon-upload">点击上传</i-button>
-    <p class="ive-upload-tips">只能上传jpg/png文件，且不超过500kb</p>
+    <p class="ive-upload-tips"><slot name="tips"></slot></p>
     <ul class="ive-upload-list">
       <li v-for="(item, index) in uploadList" :key="index">
         <Icon type="icon-file"></Icon>
-        <span>{{item.fileName}} / {{(item.size/1024).toFixed(2)}}kb</span>
-        <Icon v-if="item.progress==100" type="icon-checkmark"></Icon>
+        <span>{{item.fileName}}</span>
         <transition name="fade">
-          <i-progress v-if="item.progress<100"  width="350" height="2" :error="item.error" :percent="item.progress"></i-progress>
+          <Icon v-if="item.status=='success' && !item.error" type="icon-checkmark"></Icon>
+        </transition>
+        <transition name="fade">
+          <i-progress v-if="item.progress<100 || item.error"  width="350" height="2" :error="item.error" :percent="item.progress"></i-progress>
         </transition>
       </li>
     </ul>
@@ -28,22 +30,30 @@ export default {
     accept: {
       type: String
     },
-    name:{
-      type:String
+    name: {
+      type: String
     },
-    maxSize:{
-      type:Number
+    maxSize: {
+      type: Number
     },
-    action:{
-      type:String
+    action: {
+      type: String
     },
-    token:{
-      type:String
+    token: {
+      type: String
     }
   },
   data() {
     return {
-      uploadList:[]
+      uploadList: [
+        {
+          fileName: '121.png',
+          size: 21,
+          progress: 100,
+          error: false,
+          status: 'success'
+        }
+      ]
     }
   },
   created() {},
@@ -52,8 +62,8 @@ export default {
       this.$refs.input.click()
     },
     change(file) {
-      if(!file.target.files) return
-      if(!this.multiple){
+      if (!file.target.files) return
+      if (!this.multiple) {
         let postFiles = file.target.files[0]
         const formData = new FormData()
         formData.append('file', postFiles)
@@ -65,42 +75,43 @@ export default {
           progress: 0,
           error: false
         })
-        this.upload(formData, this.uploadList.length-1)
+        this.upload(formData, this.uploadList.length - 1)
       }
-
     },
-    upload(formdata, index){
+    upload(formdata, index) {
       let self = this
       axios({
         url: this.action,
-        method:'POST',
-        headers:{
-          'Content-Type':'multipart/form-data',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Access-Control-Allow-Origin':true,
           Authorization: `UpToken ${this.token}`
         },
-        data:formdata,
-        onUploadProgress:function(progressEvent){
-          self.$emit('progress', parseFloat(progressEvent.loaded / progressEvent.total * 100))
-          self.uploadList[index].progress = parseFloat(progressEvent.loaded / progressEvent.total * 100)
-        },
-      }).then(function(response){
-        console.log(response)
-      }).catch(function(err){
-        self.uploadList[index].progress = 1
-        self.uploadList[index].error = true
-        console.log(self.uploadList[index])
+        data: formdata,
+        onUploadProgress: function(progressEvent) {
+          let p = (progressEvent.loaded / progressEvent.total * 100).toFixed(2)
+          self.$emit('progress', parseFloat(p))
+          self.uploadList[index].progress = parseFloat(p)
+        }
       })
+        .then(function(response) {
+          console.log(response)
+          self.$emit('on-success', response)
+          self.$refs.input.value = null
+        })
+        .catch(function(err) {
+          self.$emit('on-error', err)
+          self.uploadList[index].error = true
+          self.$refs.input.value = null
+          console.log(self.uploadList[index])
+        })
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s
-}
-.fade-enter, .fade-leave-to {
-  opacity: 0
-}
+
 </style>
 
