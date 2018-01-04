a58c8a172df395ad7c11e9ed2947124febf2db36
qhx0807 <820240134@qq.com>
2018-01-04 15:48:16 +0800

    :sparkles: alert

--- packages/components/alert/alert.vue
+++ packages/components/alert/alert.vue
@@ -1,12 +1,87 @@
 <template>
   <transition name="fade">
-    <div>alert</div>
+    <div :class="wrapClass"  v-if="visable">
+      <i :class="iconClass" v-if="showIcon"></i>
+      <i v-if="closable" @click="closeHandler" class="iconfont icon-cross"></i>
+      <div class="ive-alert-content">
+        <div :class="titleClass">{{title}}
+
+        </div>
+        <div v-if="desc" class="ive-alert-desc">{{desc}}</div>
+      </div>
+    </div>
   </transition>
 </template>
 
 <script>
 export default {
-  name: 'iAlert'
+  name: 'iAlert',
+  props: {
+    title:{
+      type:String,
+    },
+    desc:{
+      type:String,
+    },
+    showIcon:{
+      type:Boolean,
+      default:false
+    },
+    closable:{
+      type:Boolean,
+      default: false
+    },
+    type:{
+      type:String,
+      default: 'info'
+    },
+    closeText:String,
+  },
+  data() {
+    return {
+      prefixCls: 'ive-alert',
+      visable: true,
+      icon: {
+        info: 'icon-iconfontinfo',
+        success: 'icon-success',
+        warning: 'icon-warning3',
+        error: 'icon-icon-test'
+      }
+    }
+  },
+  computed: {
+    wrapClass() {
+      return [
+        `${this.prefixCls}-wrap`,
+        {
+          [`${this.prefixCls}-${this.type}`] : this.type
+        }
+      ]
+    },
+    iconClass() {
+      return [
+        `iconfont`,
+        {
+          [this.icon[this.type]] : this.type,
+          [`is-big`] : !!this.desc
+        }
+      ]
+    },
+    titleClass(){
+      return [
+        `${this.prefixCls}-title`,
+        {
+          [`is-bold`] : !!this.desc
+        }
+      ]
+    }
+  },
+  methods: {
+    closeHandler(){
+      this.visable = false
+      // this.$el.addEventListener('transitionend', )
+    }
+  }
 }
 </script>
 
