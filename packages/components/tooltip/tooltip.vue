630037f1d99f0110b525543fff83a7f15d2139d6
qhx0807 <820240134@qq.com>
2018-01-05 22:52:29 +0800

    update tooltip

--- packages/components/tooltip/tooltip.vue
+++ packages/components/tooltip/tooltip.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="ive-tooltip-wrap">
-    <slot></slot>
+    <div class="ive-tooltip-el">
+      <slot></slot>
+    </div>
     <transition name="fade">
-      <div>
-        123
+      <div :class="popperClass" :placement="placement">
+          <div class="ive-tooltip-inner" v-html="content" :style="widthStyle"></div>
       </div>
     </transition>
   </div>
@@ -11,7 +13,40 @@
 
 <script>
 export default {
-  name:'Tooltip'
+  name:'Tooltip',
+  props:{
+    content: {
+      type:String,
+    },
+    placement:String,
+    width:{
+      type:[String, Number]
+    }
+  },
+  data () {
+    return {
+      visible: false,
+      prefixCls:'ive-tooltip'
+    }
+  },
+  computed: {
+    widthStyle(){
+      if(this.width){
+        return {
+          width: `${this.width}px`,
+          maxWidth: `${this.width}px`
+        }
+      }
+    },
+    popperClass(){
+      return [
+        `${this.prefixCls}-popper`
+      ]
+    }
+  },
+  methods: {
+
+  }
 }
 </script>
 
