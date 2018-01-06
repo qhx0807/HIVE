5766e874fad123c3a8236f7cd17bca229dfe8e1f
qhx0807 <820240134@qq.com>
2018-01-06 22:50:04 +0800

    update tooltip

--- packages/components/tooltip/tooltip.vue
+++ packages/components/tooltip/tooltip.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="ive-tooltip-wrap">
-    <div class="ive-tooltip-el">
+    <div class="ive-tooltip-el"
+      @mouseover="mouseoverHandler"
+      @mouseout="mouseoutHandler">
       <slot></slot>
     </div>
     <transition name="fade">
-      <div :class="popperClass" :placement="placement">
+      <div :class="popperClass" :placement="placement" v-show="visible">
           <div class="ive-tooltip-inner" v-html="content" :style="widthStyle"></div>
       </div>
     </transition>
@@ -18,7 +20,14 @@ export default {
     content: {
       type:String,
     },
-    placement:String,
+    placement:{
+      type:String,
+      default:'top-start'
+    },
+    always:{
+      type:Boolean,
+      default:false
+    },
     width:{
       type:[String, Number]
     }
@@ -27,6 +36,13 @@ export default {
     return {
       visible: false,
       prefixCls:'ive-tooltip'
+    }
+  },
+  created () {
+    if(this.always){
+      this.visible = true
+    }else{
+      this.visible = false
     }
   },
   computed: {
@@ -45,7 +61,14 @@ export default {
     }
   },
   methods: {
-
+    mouseoverHandler(){
+      this.visible = true
+    },
+    mouseoutHandler(){
+      setTimeout(()=>{
+        this.visible = false
+      },100)
+    }
   }
 }
 </script>
