fb2e888d4bba2b83fca8e52f66cf8fbb5bde4ecb
qhx0807 <820240134@qq.com>
2018-02-28 16:51:43 +0800

    :sparkles: Notification

--- packages/components/notification/index.js
+++ packages/components/notification/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Notif from './notification.vue'
 
-let ModalCstr = Vue.extend(Notif)
+let NotifyConstructor = Vue.extend(Notif)
 let instance
+let instances = []
+let count = 1
 
 let Notification = function (options) {
   options = options || {}
@@ -12,15 +14,31 @@ let Notification = function (options) {
     }
   }
 
-  instance = new ModalCstr({
+  let id = 'notification_' + count++
+  let userOnClose = options.onClose
+  let position = options.position || 'top-right'
+
+  options.onClose = function () {
+    Notification.close(id, userOnClose)
+  }
+
+  instance = new NotifyConstructor({
     data: options
   })
 
+  instance.id = id
   instance.vm = instance.$mount()
   document.body.appendChild(instance.vm.$el)
   instance.vm.visible = true
   instance.dom = instance.vm.$el
 
+  let verticalOffset = options.offset || 0
+  instances.filter(item => item.position === position).forEach(item => {
+    verticalOffset += item.$el.offsetHeight + 16
+  })
+  verticalOffset += 16
+  instance.verticalOffset = verticalOffset
+  instances.push(instance)
   return instance.vm
 }
 
@@ -38,9 +56,36 @@ type.forEach(type => {
   }
 })
 
-Notification.remove = function () {
-  instance.vm.visible = false
-  instance.destroyElement()
+Notification.close = function (id, userOnClose) {
+  let index = -1
+  const len = instances.length
+  const curInstance = instances.filter((instance, i) => {
+    if (instance.id === id) {
+      index = i
+      return true
+    }
+    return false
+  })[0]
+  if (!curInstance) return
+  if (typeof userOnClose === 'function') {
+    userOnClose(curInstance)
+  }
+  instances.splice(index, 1)
+
+  if (len <= 1) return
+  const position = curInstance.position
+  const removedHeight = curInstance.vm.$el.offsetHeight
+  for (let i = index; i < len - 1; i++) {
+    if (instances[i].position === position) {
+      instances[i].vm.$el.style[instance.verticalProperty] = parseInt(instances[i].vm.$el.style[instance.verticalProperty], 10) - removedHeight - 16 + 'px'
+    }
+  }
+}
+
+Notification.closeAll = function () {
+  for (let i = instances.length - 1; i >= 0; i--) {
+    instances[i].close()
+  }
 }
 
 export default Notification
--- packages/components/notification/notification.vue
+++ packages/components/notification/notification.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="ive-notif-wrap">123</div>
+  <transition :name="transitionName">
+    <div
+      :class="wrapClass"
+      :style="positionStyle"
+      v-if="visible"
+      @mouseenter="clearTimer"
+      @mouseleave="startTimer"
+      role="alert">
+      <i v-if="showClose" @click="close" class="iconfont icon-cross"></i>
+      <i v-if="type" :class="iconClass"></i>
+      <div class="ive-notify-group">
+        <h2 v-if="title" class="ive-notify-title">{{title}}</h2>
+        <div class="ive-notify-content">
+          <p v-html="content"></p>
+        </div>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -8,15 +25,120 @@ export default {
   props: {},
   data () {
     return {
-      preFixCls: 'ive-notif',
+      preFixCls: 'ive-notify',
+      visible: false,
+      title: '',
+      type: '',
+      showClose: true,
+      timer: null,
+      closed: false,
+      position: 'top-right',
+      icon: {
+        info: 'icon-iconfontinfo',
+        success: 'icon-success',
+        warning: 'icon-warning3',
+        error: 'icon-icon-test'
+      },
+      duration: 4500,
+      content: '',
+      transitionName: 'solidInRight',
+      verticalOffset: 0,
+      onClose: null,
     }
   },
   created () {
-
+    if (~this.position.indexOf('right')) {
+      this.transitionName = 'solidInRight'
+    }else {
+      this.transitionName = 'solidInLeft'
+    }
+  },
+  watch: {
+    closed(newVal) {
+      if (newVal) {
+        this.visible = false
+        this.destroyElement()
+        // this.$el.addEventListener('transitionend', this.destroyElement)
+      }
+    }
+  },
+  computed: {
+    iconClass() {
+      return [
+        `ive-notify-icon`,
+        `iconfont`,
+        {
+          [`${this.icon[this.type]}`] : this.type
+        }
+      ]
+    },
+    wrapClass() {
+      let clsArr = this.position.split('-')
+      return [
+        `ive-notify-wrap`,
+         ...clsArr
+      ]
+    },
+    verticalProperty() {
+      return ~this.position.indexOf('top') ? 'top' : 'bottom'
+    },
+    positionStyle() {
+      return {
+        [this.verticalProperty]: `${ this.verticalOffset }px`
+      }
+    }
+  },
+  mounted() {
+    this.startTimer()
+    document.addEventListener('keydown', this.keydown)
+  },
+  beforeDestroy () {
+    this.clearTimer()
   },
   methods: {
-
+    close() {
+      this.closed = true
+      if (typeof this.onClose === 'function') {
+        this.onClose()
+      }
+    },
+    clearTimer() {
+      clearTimeout(this.timer)
+    },
+    startTimer() {
+      if (this.duration > 0) {
+        this.timer = setTimeout(() => {
+          if (!this.closed) {
+            this.close()
+          }
+        }, this.duration)
+      }
+    },
+    keydown(e) {
+      if (e.keyCode === 27) {
+        if (!this.closed) {
+          this.close()
+        }
+      }
+    },
+    destroyElement() {
+      this.$el.removeEventListener('transitionend', this.destroyElement)
+      this.$destroy(true)
+      this.$el.parentNode.removeChild(this.$el)
+    }
   }
 }
 </script>
 
+<style lang="less" scoped>
+.solidInRight-enter{
+  right: 0;
+  opacity: 0;
+  transform: translate(100%, 0);
+}
+.solidInLeft-enter{
+  right: 0;
+  opacity: 0;
+  transform: translate(-100%, 0);
+}
+</style>
